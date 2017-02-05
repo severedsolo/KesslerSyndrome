f8d7ae59f3bf9ad7e89d012df327826a2829629d
severedsolo <evil.penguin@live.co.uk>
2017-02-05 17:21:06 +0000

    Check inclination

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿#if DEBUG
+#define ENABLE_PROFILER
+#define DEVELOPMENT
+#endif
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
